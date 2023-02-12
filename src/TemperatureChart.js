455b0c89f3a7a328c5fa2f49cc6aed87d4713309
matvey733 <67281212+matvey733@users.noreply.github.com>
2023-02-12 16:59:29 +0300

    damn square

--- src/TemperatureChart.js
+++ src/TemperatureChart.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 export default function TemperatureChart({ temperatureData }) {
   const canvasRef = useRef(null);
-  console.log(canvasRef);
+  const canvasWidth = useRef(200);
+  const canvasHeight = useRef(150);
+  
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = canvasWidth.current;
+    canvas.height = canvasHeight.current;
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillRect(0, 0, canvasWidth.current, canvasHeight.current);
+  }, [])
 
   return <canvas ref={canvasRef}></canvas>;
 }
