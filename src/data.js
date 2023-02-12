99693db26ae2892df227ea6f9296e9add10fa6c8
matvey733 <67281212+matvey733@users.noreply.github.com>
2023-02-12 19:35:18 +0300

    new data

--- src/data.js
+++ src/data.js
@@ -1,11 +1,29 @@
 const data = [
   {
-    date: new Date('February 10, 2023 18:50'),
-    temperature: 38.6,
+    date: new Date('Feb 10, 2023'),
+    temperatures: [
+      {
+        time: '10:30',
+        temperature: 36.6,
+      },
+      {
+        time: '11:30',
+        temperature: 36.9,
+      },
+    ],
   },
   {
-    date: new Date('Feb 11, 2022 18:10'),
-    temperature: 38.1,
+    date: new Date('Feb 11, 2022'),
+    temperatures: [
+      {
+        time: '12:45',
+        temperature: 37.0,
+      },
+      {
+        time: '18:20',
+        temperature: 35.9,
+      },
+    ],
   },
 ];
 
