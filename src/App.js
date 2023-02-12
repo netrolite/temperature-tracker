98d4c471d00d4ad25e685476e39d4f897fa375c2
matvey733 <67281212+matvey733@users.noreply.github.com>
2023-02-12 16:53:08 +0300

    connect app.js and temperaturechart.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './style.css';
 import data from './data.js';
+import TemperatureChart from './TemperatureChart';
 
 export default function App() {
-  data.forEach((item) => {
-    const localizedDate = item.date.toLocaleDateString();
-    const localizedTime = item.date.toLocaleTimeString();
-    console.log(localizedDate);
-    console.log(localizedTime);
-  });
+  const canvas = <TemperatureChart />;
 
   return (
     <div>
+      {canvas}
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
