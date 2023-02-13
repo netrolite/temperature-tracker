58b4694667412f0d5e8705d62275f0a365cd9f1d
matvey733 <67281212+matvey733@users.noreply.github.com>
2023-02-13 11:19:32 +0300

    typed in all the data i have

--- src/data.js
+++ src/data.js
@@ -17,6 +17,10 @@ const data = [
       {
         time: '22:15',
         value: 38.3
+      },
+      {
+        time: '22:30',
+        value: 37.7
       }
     ],
   },
@@ -24,28 +28,101 @@ const data = [
     date: new Date('Feb 11, 2022'),
     temperatures: [
       {
-        time: '12:45',
-        value: 37.0,
+        time: '07:00',
+        value: 36.6,
       },
       {
-        time: '18:20',
-        value: 35.9,
+        time: '10:00',
+        value: 36.6,
       },
+      {
+        time: '12:00',
+        value: 36.8
+      },
+      {
+        time: '13:20',
+        value: 37.1
+      },
+      {
+        time: '14:10',
+        value: 37.1
+      },
+      {
+        time: '15:50',
+        value: 37.6
+      },
+      {
+        time: '16:50',
+        value: 38.1
+      },
+      {
+        time: '18:50',
+        value: 37.3
+      },
+      {
+        time: '20:00',
+        value: 37.0
+      },
+      {
+        time: '21:20',
+        value: 36.8
+      },
+      {
+        time: '22:40',
+        value: 36.6
+      }
     ],
   },
   {
-    date: new Date('Feb 12, 2012'),
+    date: new Date('Feb 12, 2023'),
     temperatures: [
       {
-        time: '23:55',
-        value: 36.4,
+        time: '07:20',
+        value: 35.9,
       },
       {
-        time: '22:20',
-        value: 41.99999,
+        time: '14:10',
+        value: 37.0,
       },
+      {
+        time: '15:30',
+        value: 36.7
+      },
+      {
+        time: '16:40',
+        value: 36.7
+      },
+      {
+        time: '18:00',
+        value: 37.3
+      },
+      {
+        time: '18:50',
+        value: 37.0
+      },
+      {
+        time: '20:50',
+        value: 37.0
+      },
+      {
+        time: '22:00',
+        value: 37.1
+      }
     ],
   },
+  {
+    date: new Date("Feb 13, 2023"),
+    temperatures: [
+      {
+        time: "06:00",
+        value: 36.6
+      },
+      {
+        time: '08:40',
+        value: 36.6
+      }
+    ]
+  }
 ];
 
 export default data;
